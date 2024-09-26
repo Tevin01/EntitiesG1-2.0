fdca692d7a8c1c16968fe74361d6efd401a10d06
Tevin01 <tevin.eliya.bofona@cf2000.onmicrosoft.com>
2024-09-26 11:19:56 +0200

    Tryna fix things

---
+++ src/Controller/MainController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    # appel du gestionnaire de section
+    public function index(SectionRepository $sections): Response
+    {
+        return $this->render(
+            'main/index.html.twig', [
+                'title' => 'Homepage',
+                'homepage_text'=> "Nous somme le ".date('d/m/Y \à H:i'
+                ),
+                # on met dans une variable pour twig toutes les sections récupérés
+            ]
+        );
+    }
+    #[Route('/about', name: 'about_me')]
+    public function aboutMe(): Response
+    {
+        return $this->render('main/about.html.twig', [
+            'title' => 'About me',
+            'homepage_text'=> "Et je parle encore de moi !",
+        ]);
+    }
+}
